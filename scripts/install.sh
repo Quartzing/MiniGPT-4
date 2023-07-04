06e5462af2636c40d4d85196db16d4b047629d98
jian.zhang <jian.zhang.psu@gmail.com>
2023-07-04 10:37:26 +0000

    add vicuna weights downloading scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source scripts/download.sh
+
 # Get sub repos
 git lfs install
 git submodule update --init
@@ -16,33 +18,11 @@ mkdir -p $LLAMA_DIR
 mkdir -p $VICUA_DIR
 mkdir -p $MINIGPT4_DIR
 
-# Download the original llama model
-readarray -t URL_LIST < scripts/llama_urls.txt
+# Download the original llama model weights
+download_all_urls scripts/llama_urls.txt $LLAMA_DIR
 
-function download_url() {
-    local source_url=$1
-    local target_dir=$2
-    local target_path=$target_dir/${source_url##*/}
-
-    echo downloading $source_url to $target_path...
-
-    if [ -f $target_path ]; then
-        echo $target_path
-        echo $target_path already exits, skipping...
-        return
-    fi
-
-    wget $source_url -P $target_dir/
-}
-
-for url in ${URL_LIST[@]}
-do
-    download_url $url $LLAMA_DIR
-done
-
-# Download MiniGPT4 weights
-echo Downloading MiniGPT4 weights...
-download_url https://drive.google.com/file/d/1RY9jV0dyqLX-o38LrumkKRh6Jtaop58R/view?usp=sharing
+# Download the original vicuna model weights
+download_all_urls scripts/vicuna_urls.txt $VICUA_DIR
 
 # Generate vicuna weights
 python -m fastchat.model.apply_delta --base $LLAMA_DIR  --target $VICUA_DIR  --delta vicuna-7b-delta-v0/
