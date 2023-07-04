ca317448d535723c6331342187e45f1adb09c7d4
jian.zhang <jian.zhang.psu@gmail.com>
2023-07-04 11:15:09 +0000

    update requirements.txt and download logic

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,21 +2,25 @@
 source scripts/download.sh
 
 # Get sub repos
-git lfs install
+# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+# apt-get install git-lfs
+# git lfs install
 git submodule update --init
 
 # Install dependencies
 pip install -r requirements.txt
-# pip install -e FastChat
+pip install -e FastChat
 
 # Prepare dirs
 MODEL_DIR=models
 LLAMA_DIR=$MODEL_DIR/llama
 VICUA_DIR=$MODEL_DIR/vicuna
 MINIGPT4_DIR=$MODEL_DIR/minigpt4
+OUTPUT_DIR=$MODEL_DIR/output
 mkdir -p $LLAMA_DIR
 mkdir -p $VICUA_DIR
 mkdir -p $MINIGPT4_DIR
+mkdir -p $OUTPUT_DIR
 
 # Download the original llama model weights
 download_all_urls scripts/llama_urls.txt $LLAMA_DIR
@@ -25,4 +29,4 @@ download_all_urls scripts/llama_urls.txt $LLAMA_DIR
 download_all_urls scripts/vicuna_urls.txt $VICUA_DIR
 
 # Generate vicuna weights
-python -m fastchat.model.apply_delta --base $LLAMA_DIR  --target $VICUA_DIR  --delta vicuna-7b-delta-v0/
+python -m fastchat.model.apply_delta --base $LLAMA_DIR  --target $OUTPUT_DIR  --delta $VICUA_DIR
