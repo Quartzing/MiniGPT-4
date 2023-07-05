e627df81810cd1a27135f34a34c30f66082593d9
jian.zhang <jian.zhang.psu@gmail.com>
2023-07-05 06:43:17 +0000

    update partially with auto loading of vicuna delta wieghts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 source scripts/download.sh
 
-# Get sub repos
-# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-# apt-get install git-lfs
-# git lfs install
-git submodule update --init
-
-# Install dependencies
-pip install -r requirements.txt
-pip install -e FastChat
-
 # Prepare dirs
 MODEL_DIR=models
 LLAMA_DIR=$MODEL_DIR/llama
-VICUA_DIR=$MODEL_DIR/vicuna
+VICUNA_DIR=$MODEL_DIR/vicuna-7b-delta-v0
 MINIGPT4_DIR=$MODEL_DIR/minigpt4
 OUTPUT_DIR=$MODEL_DIR/output
 mkdir -p $LLAMA_DIR
-mkdir -p $VICUA_DIR
-mkdir -p $MINIGPT4_DIR
 mkdir -p $OUTPUT_DIR
 
-# Download the original llama model weights
-download_all_urls scripts/llama_urls.txt $LLAMA_DIR
+# Install dependencies
+# pip install -r requirements.txt
+# conda create -n vicuna python=3.10
+# bash && conda init bash
+# conda activate vicuna
 
-# Download the original vicuna model weights
-download_all_urls scripts/vicuna_urls.txt $VICUA_DIR
+# Download vicuna delta weights
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+apt-get install git-lfs
+git lfs install
+git -C $VICUA_DIR clone https://huggingface.co/lmsys/vicuna-7b-delta-v0
+
+# Download the original llama model weights
+# pip install pyllama
+# python -m llama.download --model_size 7B --folder $LLAMA_DIR
+download_all_urls $PWD/scripts/llama_urls.txt $LLAMA_DIR
 
 # Generate vicuna weights
+pip install fschat==0.1.1
 python -m fastchat.model.apply_delta --base $LLAMA_DIR  --target $OUTPUT_DIR  --delta $VICUA_DIR
