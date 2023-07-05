f5dd1326defca13fed20ba20f11baa7e95b8653a
jian.zhang <jian.zhang.psu@gmail.com>
2023-07-05 07:43:57 +0000

    fix llama tokenizer naming issue in llama/tokenizer_config.json

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,24 +10,14 @@ OUTPUT_DIR=$MODEL_DIR/output
 mkdir -p $LLAMA_DIR
 mkdir -p $OUTPUT_DIR
 
-# Install dependencies
-# pip install -r requirements.txt
-# conda create -n vicuna python=3.10
-# bash && conda init bash
-# conda activate vicuna
-
-# Download vicuna delta weights
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-apt-get install git-lfs
-git lfs install
-git -C $VICUNA_DIR clone https://huggingface.co/lmsys/vicuna-7b-delta-v0
-
 # Download the original llama model weights
-# pip install pyllama
-# python -m llama.download --model_size 7B --folder $LLAMA_DIR
 download_all_urls $PWD/scripts/llama_urls.txt $LLAMA_DIR
+
+# Download the vicuna delta weights
+download_all_urls $PWD/scripts/vicuna_urls.txt $VICUNA_DIR
 
 # Generate vicuna weights
 git submodule update --init
 pip install -e FastChat
 python -m fastchat.model.apply_delta --base-model-path $LLAMA_DIR  --target-model-path $OUTPUT_DIR  --delta-path $VICUNA_DIR
+ 
